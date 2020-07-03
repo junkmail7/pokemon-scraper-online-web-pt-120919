e5bdd8c16e841f10a69536564434c12023da0a43
William Pannell <lpann5@gmail.com>
2020-07-03 22:02:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
   def initialize(id: , name:, type:, db:)
-    @id = idea
+    @id = id
     @name = name
     @type = type
     @db = db
